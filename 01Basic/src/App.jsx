2b0748c0a6b39728d460742c7eb93bc6d6d0c97f
Atif Afridi <atifafridi378@gmail.com>
2025-01-04 21:42:50 +0500

    init

--- 01Basic/src/App.jsx
+++ 01Basic/src/App.jsx
@@ -1,10 +1,14 @@
+import Root from "./Root"
 
 
 function App() {
  
   return (
+   <>
+   
    <h1 >Chai aur code | Learing React | Basics </h1>
-  
+   <Root/>
+   </>
 
   )
 }
---
+++ 01Basic/src/Root.jsx
@@ -0,0 +1,10 @@
+
+function Root(){
+
+return(
+ <h1>  Rooot folder for learning React from Chai aur Code  </h1>
+)   
+
+}
+
+export default Root
