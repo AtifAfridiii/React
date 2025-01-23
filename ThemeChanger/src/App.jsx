f29943a9896160df7b538462c27d575c66c0debe
Atif Afridi <atifafridi378@gmail.com>
2025-01-23 23:48:55 +0500

    init

--- ThemeChanger/src/App.jsx
+++ ThemeChanger/src/App.jsx
@@ -1,11 +1,29 @@
-
+import { useEffect, useState } from "react"
+import { ThememodeProvider } from "./context"
+import React from "react"
 
 function App() {
   
+  const [theme , setTheme ] = useState('light')
+   
+  function LightTheme(){
+    setTheme('light')
+   }
+
+   function DarkTheme(){
+    setTheme('dark')
+   }
+
+
+   useEffect(()=>{
+    const 
+   })
+
+
   return (
     <>
-     
-<div className="flex flex-wrap min-h-screen items-center">
+   <ThememodeProvider value={{theme,DarkTheme,LightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                         {/* Theme btn */}
@@ -16,6 +34,7 @@ function App() {
                     </div>
                 </div>
             </div>
+</ThememodeProvider>  
 
     </>
   )
