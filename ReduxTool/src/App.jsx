c70e5b2aaf3774f15c628b695104b12cca1e5a77
Atif Afridi <atifafridi378@gmail.com>
2025-02-08 23:15:59 +0500

    init

--- ReduxTool/src/App.jsx
+++ ReduxTool/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import React from 'react'
+import AddTodo from './app/components/AddTodo'
+import Todos from './app/components/Todo'
 
 function App() {
 
   return (
     <>
     <h1>Hello</h1>
+    <AddTodo/>
+    <Todos/>
+
     </>
   )
 }
